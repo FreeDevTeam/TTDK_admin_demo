50d5d2e6d21a6ad543f0b5d5af05d7e5726df5a9
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:41:31 +0700

    Initialize nguyen/gui_thong_bao_khi_ghi_chu_cho_don_tu_van

---
+++ src/views/ui-elements/cards/statistics/TotalDeployedStation.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Home, Loader } from 'react-feather'
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { injectIntl } from 'react-intl'
+import "./index.scss";
+import { Card } from 'reactstrap'
+import { formatDisplayNumber } from '../../../../utility/Utils'
+import SpinnerTextAlignment from '../../../components/spinners/SpinnerTextAlignment'
+
+const TotalDeployedStation = ({ kFormatter, intl, data }) => {
+  let total = 0
+  const newArray = data?.map(value =>{
+    const isBool = value.stationBookingConfig?.some((item) => item.enableBooking  === 1);
+    if(isBool === true){
+      total ++
+    }
+  })
+  return data !== null ? (
+    <StatsWithAreaChart
+      icon={<Home size={21} />}
+      stats={formatDisplayNumber(total)}
+      color='success'
+      statTitle={intl.formatMessage({id: "totalDeployedStation"})}
+      // className='col-widget'
+      height='15'
+    />
+  ) : <Card  className='col-widget d-flex justify-content-center align-items-center'>
+  <SpinnerTextAlignment size={60} />
+</Card>
+}
+
+export default injectIntl(TotalDeployedStation)
