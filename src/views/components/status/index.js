0c6398114089f468baafe84678c9c97bc470e3a3
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:30:40 +0700

    Initialize tai/khoi_luong_=_0

---
+++ src/views/components/status/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './status.module.scss'
+import { injectIntl } from 'react-intl'
+
+const Status = ({ intl, params }) => {
+  return (
+    <div>
+      {params >= 2 ? (
+        <div>
+          <span className="bg-danger rounded pt-1 pr-1 pb-1 pl-1 text-white">
+            {intl.formatMessage({ id: 'Off' })}
+            {' : '}
+            {params} {intl.formatMessage({ id: 'day' })}
+          </span>
+        </div>
+      ) : (
+        <span className="bg-success rounded pt-1 pr-1 pb-1 pl-1 text-white">{intl.formatMessage({ id: 'actived' })}</span>
+      )}
+    </div>
+  )
+}
+
+export default injectIntl(Status)
