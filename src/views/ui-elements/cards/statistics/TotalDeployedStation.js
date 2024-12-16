a054936308533f7774b6919f751b5a2c2b9977f3
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:26:57 +0700

    Initialize tai/trang_chu_xoa_useffect

---
+++ src/views/ui-elements/cards/statistics/TotalDeployedStation.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Home, Loader } from 'react-feather'
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { injectIntl } from 'react-intl'
+import "./index.scss";
+import { Card } from 'reactstrap'
+
+const TotalDeployedStation = ({ kFormatter, intl, data }) => {
+  let total = 0
+  const newArray = data.map(value =>{
+    const isBool = value.stationBookingConfig.some((item) => item.enableBooking  === 1);
+    if(isBool === true){
+      total ++
+    }
+  })
+  return data !== null ? (
+    <StatsWithAreaChart
+      icon={<Home size={21} />}
+      stats={total}
+      color='success'
+      statTitle={intl.formatMessage({id: "totalDeployedStation"})}
+      className='col-widget'
+    />
+  ) : <Card  className='col-widget d-flex justify-content-center align-items-center'>
+  <Loader size={60} />
+</Card>
+}
+
+export default injectIntl(TotalDeployedStation)
