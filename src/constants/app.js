6906057e93d4341d86985edc6c98a6450740dffe
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:33:11 +0700

    Initialize tai/tram_xoa_popup_

---
+++ src/constants/app.js
@@ -0,0 +1,87 @@
+export const FETCH_APPS_REQUEST = 'FETCH_APPS_REQUEST'
+export const FETCH_APPS_SUCCESS = 'FETCH_APPS_SUCCESS'
+export const FETCH_APP_CHANGE = 'FETCH_APP_CHANGE'
+
+export const VEHICLE_TYPE = {
+  CAR : 1,
+  OTHER : 10,
+  RO_MOOC : 20
+}
+
+export const SCHEDULE_STATUS = {
+  NEW: 0,
+  CONFIRMED: 10,
+  CANCELED: 20,
+  CLOSED: 30,
+}
+
+export const LICENSEPLATES_COLOR = {
+  white: 1,
+  blue: 2,
+  yellow: 3,
+  red: 4,
+}
+
+export const LOCAL = {
+  normal : 1,
+  high_level : 2
+}
+
+export const APP_USER_ROLE  = {
+  patern : 1,
+  technician : 2,
+  technicians_senior : 3,
+  accountant : 4
+}
+
+export const VEHICLEVERIFIEDINFO = {
+  NOT_VERIFIED: 0, // chưa kiểm tra
+  VERIFIED: 1,  // đã kt và có dữ liệu
+  VERIFIED_BUT_NO_DATA: -1, // đã kt nhưng ko có dữ liệu
+  VERIFIED_BUT_WRONG_EXPIRE_DATE: -2, // đã kt nhưng sai ngày hết hạn
+  VERIFIED_BUT_WRONG_VEHICLE_TYPE: -10, //Đã kiểm tra nhưng sai loai phuong tien
+  VERIFIED_BUT_ERROR: -3, //Đã kiểm tra nhưng thất bại
+  NOT_VALID_SERIAL: -20, // số seri GCN ko hợp lệ
+}
+
+export const SCHEDULE_TYPE = {
+  VEHICLE_INSPECTION: 1, // Đăng kiểm xe cũ
+  NEW_VEHICLE_INSPECTION: 2, // đăng kiểm xe mới
+  REGISTER_NEW_VEHICLE: 3, // Nộp hồ sơ xe mới
+}
+
+export const CUSTOMER_RECEIPT_STATUS = {
+  NEW: 'New', // chưa thanh toán
+  PENDING: 'Pending', // đang xử lý
+  FAILED: 'Failed', // thanh toán thất bại
+  SUCCESS: 'Success', // thanh toán thành công
+  CANCELED: 'Canceled', // đã hủy
+}
+
+export const PAYMENT_TYPES =  {
+  CASH: 1, // tiền mặt
+  BANK_TRANSFER: 2, // chuyển khoản
+  VNPAY: 3, // vnpay
+  CREDIT_CARD: 4, // thẻ tín dụng
+  MOMO: 5, // momo
+}
+
+export const FUEL_TYPE = {
+  GASOLINE : 1,
+  OIL : 2
+}
+
+export const DEVICE_STATUS = {
+  NEW : 'NEW',
+  ACTIVE : 'ACTIVE',
+  MAINTENANCE : 'MAINTENANCE',
+  INACTIVE : 'INACTIVE'
+}
+
+export const DOCUMENT_CATEGORY = {
+  OFFICIAL_LETTER: 1, //Công văn
+  ESTABLISHMENT_APPOINTMENT_DOCUMENT: 2, // Giấy tờ thành lập / bổ nhiệm 
+  PERIODIC_INSPECTION_DOCUMENT: 3, //Văn bản kiểm tra định kỳ
+  TASK_ASSIGNMENT_FORM: 4 // Phiếu phân công nhiệm vụ
+}
+
---
+++ src/views/pages/station/addDevice.js
@@ -0,0 +1,240 @@
+import '@styles/react/libs/flatpickr/flatpickr.scss'
+import moment from 'moment'
+import React, { Fragment, memo, useState } from 'react'
+import { ChevronLeft } from 'react-feather'
+import Flatpickr from 'react-flatpickr'
+import { useForm } from 'react-hook-form'
+import { injectIntl } from 'react-intl'
+import { useHistory } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import {
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    CardText,
+    Col,
+    Form,
+    FormGroup,
+    Input,
+    Label,
+    Row
+} from 'reactstrap'
+import './index.scss'
+import { readAllStationsDataFromLocal } from '../../../helper/localStorage'
+import { readAllArea } from '../../../helper/localStorage'
+import StationDevice from '../../../services/statiosDevice'
+
+const getYearOptions = () => {
+  const currentYear = new Date().getFullYear();
+  const startYear = 1990; // Năm bắt đầu
+  const years = [];
+  for (let year = currentYear; year >= startYear; year--) {
+    years.push(year.toString());
+  }
+  return years;
+};
+
+const deviceOptions = [
+  {value : 'NEW', label : 'new'},
+  {value : 'ACTIVE', label : 'active'},
+  {value : 'MAINTENANCE', label : 'maintenance'},
+  {value : 'INACTIVE', label : 'inactive'},
+]
+
+const AddDevice = ({ intl }) => {
+  const history = useHistory()
+
+  const { register,errors, handleSubmit } = useForm({
+    mode: 'onSubmit',
+    defaultValues: {}
+  })
+  const [userData, setUserData] = useState({})
+  const [userDataTouched, setUserDataTouched] = useState({})
+  const [date, setDate] = useState('')
+  const readLocal = readAllStationsDataFromLocal()
+  const readArea = readAllArea()
+  const listStation = readLocal.sort((a, b) => a - b)
+
+  function handleInsert(data) {
+    StationDevice.handleInsert(data).then((res) => {
+      if (res) {
+        const { statusCode, message } = res
+        if (statusCode === 200) {
+          toast.success(intl.formatMessage({ id: 'actionSuccess' }, { action: intl.formatMessage({ id: 'add_new' }) }))
+        } else {
+          toast.warn(intl.formatMessage({ id: 'actionFailed' }, { action: intl.formatMessage({ id: 'add_new' }) }))
+        }
+      }
+    })
+  }
+
+  const handleOnchange = (name, value) => {
+    setUserData({
+      ...userData,
+      [name]: value
+    })
+    setUserDataTouched({
+      ...userDataTouched,
+      [name]: value
+    })
+  }
+
+  return (
+    <Fragment>
+      <Row>
+        <Col sm="8" xs="12">
+          <Card>
+            <div className="pt-1 pl-1 pointer" onClick={history.goBack}>
+              <ChevronLeft />
+              {intl.formatMessage({ id: 'goBack' })}
+            </div>
+            <CardHeader className="justify-content-center flex-column">
+              <CardText className="h3">{intl.formatMessage({ id: 'add_device' })}</CardText>
+            </CardHeader>
+            <hr color="#808080" />
+            <CardBody className="justify-content-center flex-column">
+              <Form
+                onSubmit={handleSubmit((data) => {
+                    handleInsert({
+                      deviceName : userDataTouched.deviceName,
+                      stationsId: userDataTouched.stationsId,
+                      deviceBrand: userDataTouched.deviceBrand,
+                      deviceManufactureYear: userDataTouched.deviceManufactureYear,
+                      deviceType: userDataTouched.deviceType,
+                      deviceSeri: userDataTouched.deviceSeri,
+                      deviceStatus : userDataTouched.deviceStatus
+                  })
+                })}>
+                <Row>
+                  <Col sm="6" xs="12">
+                    <FormGroup>
+                      <Label for="stationCode" className='text-small'>{intl.formatMessage({ id: 'stationCode' })} <span style={{color:"red"}}>*</span></Label>
+                      <Input
+                        type="select"
+                        id="stationsId"
+                        name="stationsId"
+                        bsSize="md"
+                        innerRef={register({ required: true })}
+                        invalid={errors.stationsId && true}
+                        value={userData.stationsId || ''}
+                        onChange={(e) => {
+                          const { name, value } = e.target
+                          handleOnchange(name, value)
+                        }}>
+                        <option value={''}>{intl.formatMessage({ id: 'chose' })}</option>
+                        {listStation?.map((item) => {
+                          return <option value={item.stationsId}>{item.stationCode}</option>
+                        })}
+                      </Input>
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="chain_type" className='text-small'>{intl.formatMessage({ id: 'device_name' })}</Label>
+                      <Input
+                        id="deviceName"
+                        name="deviceName"
+                        innerRef={register()}
+                        value={userData.deviceName || ''}
+                        onChange={(e) => {
+                          const { name, value } = e.target
+                          handleOnchange(name, value)
+                        }}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="chain_type" className='text-small'>{intl.formatMessage({ id: 'status' })} <span style={{color:"red"}}>*</span></Label>
+                      <Input
+                        type="select"
+                        id="deviceStatus"
+                        name="deviceStatus"
+                        bsSize="md"
+                        innerRef={register({ required: true })}
+                        invalid={errors.deviceStatus && true}
+                        value={userData.deviceStatus || ''}
+                        onChange={(e) => {
+                          const { name, value } = e.target
+                          handleOnchange(name, value)
+                        }}>
+                        <option value={''}>{intl.formatMessage({ id: 'chose' })}</option>
+                        {deviceOptions.map((item) => {
+                          return <option value={item.value}>{intl.formatMessage({ id: item.label })}</option>
+                        })}
+                      </Input>
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="firm" className='text-small'>{intl.formatMessage({ id: 'chain_type' })}</Label>
+                      <Input
+                        id="deviceType"
+                        name="deviceType"
+                        innerRef={register()}
+                        value={userData.deviceType || ''}
+                        onChange={(e) => {
+                          const { name, value } = e.target
+                          handleOnchange(name, value)
+                        }}
+                      />
+                    </FormGroup>
+                  </Col>
+                  <Col sm="6" xs="12">
+                    <FormGroup>
+                      <Label for="supply_company" className='text-small'>{intl.formatMessage({ id: 'serial_number' })}</Label>
+                      <Input
+                        id="deviceSeri"
+                        name="deviceSeri"
+                        innerRef={register()}
+                        value={userData.deviceSeri || ''}
+                        onChange={(e) => {
+                          const { name, value } = e.target
+                          handleOnchange(name, value)
+                        }}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="original_price" className='text-small'>{intl.formatMessage({ id: 'brand' })}</Label>
+                      <Input
+                        id="deviceBrand"
+                        name="deviceBrand"
+                        innerRef={register()}
+                        value={userData.deviceBrand || ''}
+                        onChange={(e) => {
+                          const { name, value } = e.target
+                          handleOnchange(name, value)
+                        }}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="year_purchase" className='text-small'>{intl.formatMessage({ id: 'year_manufacture' })}</Label>
+                      <Input
+                        type="select"
+                        id="deviceManufactureYear"
+                        name="deviceManufactureYear"
+                        bsSize="md"
+                        innerRef={register({ required: true })}
+                        value={userData.deviceManufactureYear || ''}
+                        onChange={(e) => {
+                          const { name, value } = e.target
+                          handleOnchange(name, value)
+                        }}>
+                        <option value={''}>{intl.formatMessage({ id: 'chose' })}</option>
+                        {getYearOptions()?.map((item) => {
+                          return <option value={item}>{item}</option>
+                        })}
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <FormGroup className="d-flex mb-0 justify-content-center">
+                  <Button.Ripple className="mr-1" color="primary" type="submit">
+                    {intl.formatMessage({ id: 'submit' })}
+                  </Button.Ripple>
+                </FormGroup>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Fragment>
+  )
+}
+
+export default injectIntl(memo(AddDevice))
