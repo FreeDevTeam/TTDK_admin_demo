4580dd7175e7ad7dbf52d0d256e3f63a35c9d509
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:30:30 +0700

    Initialize tai/thanh_toan_hóa_đơn

---
+++ src/navigation/vertical/pages.js
@@ -0,0 +1,147 @@
+// @ts-nocheck
+/* eslint-disable import/no-anonymous-default-export */
+import {Circle, DollarSign, List, Slack, BookOpen, User, Divide, Shield, HardDrive, Cast, Image, Settings, Book, MessageCircle, Cpu,Bell } from 'react-feather'
+const user = JSON.parse(localStorage.getItem("TTDK_ADMIN_WEB_userData")) || []
+
+ const App = [
+  {
+    id: 'station',
+    title: 'stations',
+    icon: <HardDrive size={12} />,
+    permissions: 'MANAGE_STATION',
+    navLink: '/pages/station'
+  },
+  {
+    id: 'account-admin',
+    title: 'admin',
+    icon: <Shield size={12} />,
+    permissions: 'MANAGE_STAFF',
+    navLink: '/pages/account-admin'
+  },
+  {
+    id: 'user',
+    title: 'User',
+    icon: <User size={12} />,
+    permissions: 'MANAGE_USER',
+    navLink: '/pages/users'
+  },
+  {
+    id: 'vehicle',
+    title: 'Vehicle',
+    icon: <Cpu size={12} />,
+    permissions: 'MANAGE_VEHICLE',
+    navLink: '/pages/vehicle'
+  },
+  {
+    id: 'schedules',
+    title: 'schedules',
+    icon: <Book size={12} />,
+    // permissions: 'MANAGE_SCHEDULES',
+    permissions: [],
+    navLink: '/pages/schedule'
+  },
+  {
+    id: 'documentary',
+    title: 'documentary',
+    icon: <BookOpen size={12} />,
+    permissions: 'MANAGE_DOCUMENT',
+    navLink: '/pages/documentary'
+  },
+  {
+    id: 'technicians',
+    title: 'technicians',
+    icon: <Slack size={12} />,
+    permissions: 'MANAGE_STATION_STAFF',
+    navLink: '/pages/technicians'
+  },
+  {
+    id: 'center_staff',
+    title: 'staff',
+    icon: <Cast size={12} />,
+    permissions: 'MANAGE_STATION_USER',
+    navLink: '/pages/center-staff'
+  },
+  {
+    id: 'report',
+    title: 'report',
+    icon: <List size={12} />,
+    permissions: [],
+    navLink: '/pages/report'
+  },
+  // {
+  //   id: 'advertising',
+  //   title: 'advertising',
+  //   icon: <Image size={12} />,
+  //   permissions: [],
+  //   navLink: '/pages/advertising'
+  // },
+  {
+    id: 'Chat',
+    title: 'Chat',
+    icon: <MessageCircle size={12} />,
+    permissions: 'MANAGE_CHAT',
+    navLink: '/apps/chat'
+  },
+  {
+    id: 'notification',
+    title: 'notification',
+    icon: <Bell size={12} />,
+    permissions: 'MANAGE_NOTIFICATION',
+    navLink: '/pages/notification'
+  },
+  {
+    id: 'device',
+    title: 'devices',
+    icon: <Divide size={12} />,
+    permissions: [],
+    navLink: '/pages/devices'
+  },
+  {
+    id: 'pay',
+    title: 'pay',
+    icon: <DollarSign size={12} />,
+    permissions: 'MANAGE_STATION_PAYMENT',
+    badge: 'primary',
+    children: [
+      {
+        id: 'schedule-pay',
+        title: 'schedules',
+        icon: <Circle size={12} />,
+        navLink: '/pages/schedule-pay'
+      },
+      {
+        id: 'bill-pay',
+        title: 'bill',
+        icon: <Circle size={12} />,
+        navLink: '/pages/bill-pay'
+      },
+      // {
+      //   id: 'Staking',
+      //   title: 'Người vay',
+      //   icon: <Circle size={12} />,
+      //   // navLink: '/pages/Stacking'
+      // },
+    ]
+  },
+]
+
+const newApp = App.map(item =>{
+  const b = user.permissions?.indexOf(item.permissions)
+  if( b === -1 ){
+    delete item.id
+    delete item.title
+    delete item.icon
+    delete item.permissions
+    delete item.navLink
+  }
+  return {...item}
+})
+
+const newArr = newApp?.filter(el => {
+  const check = Object.keys(el).length
+  if(check > 0){
+    return el
+  }
+} )
+
+export default newArr
---
+++ src/views/pages/schedule/formSchedule.js
@@ -0,0 +1,277 @@
+import React, { Fragment, useState, memo, useEffect } from 'react'
+import { ChevronLeft } from 'react-feather'
+import { injectIntl } from 'react-intl'
+import { useHistory } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import {
+  Card,
+  Input,
+  Label,
+  Row,
+  Col,
+  Button,
+  FormGroup,
+  Form,
+  Nav,
+  NavLink,
+  NavItem,
+  TabContent,
+  TabPane,
+  CardHeader,
+  CardBody,
+  CardImg,
+  CardText,
+  Badge
+} from 'reactstrap'
+import { useForm } from 'react-hook-form'
+import Service from '../../../services/request'
+import { toast } from 'react-toastify'
+import { MoreVertical, Edit, Lock, Shield, RotateCcw } from 'react-feather'
+import DataTable from 'react-data-table-component'
+import { ChevronDown } from 'react-feather'
+import ReactPaginate from 'react-paginate'
+import UserService from '../../../services/userService'
+import { LICENSEPLATES_COLOR } from './../../../constants/app'
+import { SCHEDULE_STATUS } from './../../../constants/app'
+import { VEHICLE_TYPE } from './../../../constants/app'
+import { SCHEDULE_TYPE } from '../../../constants/app'
+import { CUSTOMER_RECEIPT_STATUS } from './../../../constants/app'
+import './index.scss'
+
+const FormSchedule = ({ intl }) => {
+  const location = useLocation()
+  const history = useHistory()
+  const {
+    stationsAddress,
+    fullnameSchedule,
+    phone,
+    scheduleType,
+    licensePlates,
+    vehicleType,
+    dateSchedule,
+    time,
+    CustomerScheduleStatus,
+    scheduleCode,
+    scheduleNote,
+    licensePlateColor,
+    stationCode,
+    paymentStatus,
+    changeHistory
+  } = location.state
+
+  const { register, handleSubmit } = useForm({
+    mode: 'onSubmit',
+    defaultValues: {}
+  })
+
+  return (
+    <Fragment>
+      <Card>
+        <div className="pt-1 pl-1 pointer" onClick={history.goBack}>
+          <ChevronLeft />
+          {intl.formatMessage({ id: 'goBack' })}
+        </div>
+        <Row>
+          <Col sm="12" xs="12">
+            <Card className="mt-2">
+              <CardHeader className="justify-content-center flex-column">
+                <CardText className="h3">{intl.formatMessage({ id: 'schedule_information' })}</CardText>
+              </CardHeader>
+              <hr color="#808080" />
+              <CardBody className="justify-content-center flex-column">
+                <Row>
+                  <Col sm="6" sx="12">
+                    <FormGroup>
+                      <Label className="label_color">{intl.formatMessage({ id: 'place_of_booking' })}</Label>
+                      <p>
+                        {stationCode} - {stationsAddress}
+                      </p>
+                    </FormGroup>
+
+                    <FormGroup>
+                      <Label className="label_color">{intl.formatMessage({ id: 'services' })}</Label>
+                      <CardText>
+                        {scheduleType === SCHEDULE_TYPE.VEHICLE_INSPECTION ? (
+                          <Badge color="light-success" className="size_text">
+                            {intl.formatMessage({ id: 'old_car_registration' })}
+                          </Badge>
+                        ) : scheduleType === SCHEDULE_TYPE.NEW_VEHICLE_INSPECTION ? (
+                          <Badge color="light-danger" className="size_text">
+                            {intl.formatMessage({ id: 'new_car_registration' })}
+                          </Badge>
+                        ) : scheduleType === SCHEDULE_TYPE.REGISTER_NEW_VEHICLE ? (
+                          <Badge color="light-info" className="size_text">
+                            {intl.formatMessage({ id: 'new_car_profile' })}
+                          </Badge>
+                        ) : (
+                          '-'
+                        )}
+                      </CardText>
+                    </FormGroup>
+                    <Row>
+                      <Col sm="6">
+                        <Label className="label_color">{intl.formatMessage({ id: 'firstName' })}</Label>
+                        <p>{fullnameSchedule}</p>
+                      </Col>
+                      <Col sm="6">
+                        <Label className="label_color">{intl.formatMessage({ id: 'phoneNumber' })}</Label>
+                        <p>{phone}</p>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col sm="6">
+                        <Label className="label_color">{intl.formatMessage({ id: 'messagesDetail-customerMessagePlateNumber' })}</Label>
+                        <CardText
+                          className={`color_licensePlates 
+                          ${licensePlateColor === LICENSEPLATES_COLOR.white ? 'color_white' : ' '}
+                          ${licensePlateColor === LICENSEPLATES_COLOR.blue ? 'color_blue' : ' '}
+                          ${licensePlateColor === LICENSEPLATES_COLOR.yellow ? 'color_yellow' : ' '}
+                          ${licensePlateColor === LICENSEPLATES_COLOR.red ? 'color_red' : ' '}
+                        `}>
+                          {licensePlates}
+                        </CardText>
+                      </Col>
+                      <Col sm="6">
+                        <Label className="label_color">{intl.formatMessage({ id: 'transportation' })}</Label>
+                        <CardText>
+                          {vehicleType === VEHICLE_TYPE.CAR ? (
+                            <Badge color="light-success" className="size_text">
+                              {intl.formatMessage({ id: 'car' })}
+                            </Badge>
+                          ) : vehicleType === VEHICLE_TYPE.OTHER ? (
+                            <Badge color="light-danger" className="size_text">
+                              {intl.formatMessage({ id: 'other' })}
+                            </Badge>
+                          ) : (
+                            <Badge color="light-info" className="size_text">
+                              {intl.formatMessage({ id: 'ro_mooc' })}
+                            </Badge>
+                          )}
+                        </CardText>
+                      </Col>
+                    </Row>
+                    <Row className="mt-1">
+                      <Col sm="6">
+                        <Label className="label_color">{intl.formatMessage({ id: 'day' })}</Label>
+                        <p>{dateSchedule}</p>
+                      </Col>
+                      <Col sm="6">
+                        <Label className="label_color">{intl.formatMessage({ id: 'hour' })}</Label>
+                        <p>{time}</p>
+                      </Col>
+                    </Row>
+                    <FormGroup>
+                      <Label className="label_color">{intl.formatMessage({ id: 'messageStatus' })}</Label>
+                      <CardText>
+                        {CustomerScheduleStatus === SCHEDULE_STATUS.NEW ? (
+                          <Badge color="light-info" className="size_text">
+                            {intl.formatMessage({ id: 'unconfimred' })}
+                          </Badge>
+                        ) : CustomerScheduleStatus === SCHEDULE_STATUS.CONFIRMED ? (
+                          <Badge color="light-warning" className="size_text">
+                            {intl.formatMessage({ id: 'confirmed' })}
+                          </Badge>
+                        ) : CustomerScheduleStatus === SCHEDULE_STATUS.CANCELED ? (
+                          <Badge color="light-danger" className="size_text">
+                            {intl.formatMessage({ id: 'canceled' })}
+                          </Badge>
+                        ) : (
+                          <Badge color="light-success" className="size_text">
+                            {intl.formatMessage({ id: 'closed' })}
+                          </Badge>
+                        )}
+                      </CardText>
+                    </FormGroup>
+
+                    <FormGroup>
+                      <Label className="label_color">{intl.formatMessage({ id: 'code_schedule' })}</Label>
+                      <p>{scheduleCode}</p>
+                    </FormGroup>
+
+                    <FormGroup>
+                      <Label className="label_color">{intl.formatMessage({ id: 'pay' })}</Label>
+                      <CardText>
+                        {paymentStatus === CUSTOMER_RECEIPT_STATUS.NEW ? (
+                          <Badge color="light-info" className="size_text">
+                            {intl.formatMessage({ id: 'unpaid' })}
+                          </Badge>
+                        ) : paymentStatus === CUSTOMER_RECEIPT_STATUS.PENDING ? (
+                          <Badge color="light-warning" className="size_text">
+                            {intl.formatMessage({ id: 'processing-contract' })}
+                          </Badge>
+                        ) : paymentStatus === CUSTOMER_RECEIPT_STATUS.FAILED ? (
+                          <Badge color="light-danger" className="size_text">
+                            {intl.formatMessage({ id: 'payment-failed' })}
+                          </Badge>
+                        ) : paymentStatus === CUSTOMER_RECEIPT_STATUS.SUCCESS ? (
+                          <Badge color="light-success" className="size_text">
+                            {intl.formatMessage({ id: 'payment-success' })}
+                          </Badge>
+                        ) : paymentStatus === CUSTOMER_RECEIPT_STATUS.CANCELED ? (
+                          <Badge color="light-primary" className="size_text">
+                            {intl.formatMessage({ id: 'canceled' })}
+                          </Badge>
+                        ) : (
+                          ''
+                        )}
+                      </CardText>
+                    </FormGroup>
+
+                    <FormGroup>
+                      <Label className="label_color">{intl.formatMessage({ id: 'stationsNote' })}</Label>
+                      <p>{scheduleNote}</p>
+                    </FormGroup>
+                  </Col>
+                  <Col sm="6" sx="12">
+                    <FormGroup>
+                      <Label className="label_color">{intl.formatMessage({ id: 'history-change' })}</Label>
+                      {/* <p className="mb-0">
+                        {changeHistory && changeHistory[0].createdAt} - Đổi từ Trạng thái lịch hẹn {
+                          changeHistory && changeHistory[0].dataValueBefore === SCHEDULE_STATUS.NEW ? (
+                            <Badge color="light-info" className="size_text">
+                              {intl.formatMessage({ id: 'unconfimred' })}
+                            </Badge>
+                          ) : CustomerScheduleStatus === SCHEDULE_STATUS.CONFIRMED ? (
+                            <Badge color="light-warning" className="size_text">
+                              {intl.formatMessage({ id: 'confirmed' })}
+                            </Badge>
+                          ) : CustomerScheduleStatus === SCHEDULE_STATUS.CANCELED ? (
+                            <Badge color="light-danger" className="size_text">
+                              {intl.formatMessage({ id: 'canceled' })}
+                            </Badge>
+                          ) : (
+                            <Badge color="light-success" className="size_text">
+                              {intl.formatMessage({ id: 'closed' })}
+                            </Badge>
+                        )} sang {
+                          changeHistory && changeHistory[0].dataValueAfter === SCHEDULE_STATUS.NEW ? (
+                            <Badge color="light-info" className="size_text">
+                              {intl.formatMessage({ id: 'unconfimred' })}
+                            </Badge>
+                          ) : CustomerScheduleStatus === SCHEDULE_STATUS.CONFIRMED ? (
+                            <Badge color="light-warning" className="size_text">
+                              {intl.formatMessage({ id: 'confirmed' })}
+                            </Badge>
+                          ) : CustomerScheduleStatus === SCHEDULE_STATUS.CANCELED ? (
+                            <Badge color="light-danger" className="size_text">
+                              {intl.formatMessage({ id: 'canceled' })}
+                            </Badge>
+                          ) : (
+                            <Badge color="light-success" className="size_text">
+                              {intl.formatMessage({ id: 'closed' })}
+                            </Badge>
+                        )}
+                      </p> */}
+                    </FormGroup>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Card>
+    </Fragment>
+  )
+}
+
+export default injectIntl(memo(FormSchedule))
