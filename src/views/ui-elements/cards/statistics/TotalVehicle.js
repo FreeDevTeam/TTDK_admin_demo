8673c9c98bde4d95d70818d1de2ce34c40f62a61
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:23:00 +0700

    Initialize tai/nguoi_dung_them_bo_loc_goi_api

---
+++ src/views/ui-elements/cards/statistics/TotalVehicle.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Command } from 'react-feather'
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { injectIntl } from 'react-intl'
+
+const TotalVehicle = ({ kFormatter, intl, data }) => {
+  return data !== null ? (
+    <StatsWithAreaChart
+      icon={<Command size={21} />}
+      color='warning'
+      stats={kFormatter(data.totalVehicle )}
+      statTitle={intl.formatMessage({id: "totalVehicle"})}
+      className='col-widget'
+    />
+  ) : null
+}
+
+export default injectIntl(TotalVehicle)
