dc4b86ed6389cf1ecdfd89c38eeffadc8d72ff89
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:38:04 +0700

    Initialize nguyen/them_ui_toggle_setting_tram

---
+++ src/views/ui-elements/cards/statistics/AccountWeek.js
@@ -0,0 +1,23 @@
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { Box } from 'react-feather'
+import { injectIntl } from 'react-intl'
+import { Card } from 'reactstrap'
+import { formatDisplayNumber } from '../../../../utility/Utils'
+import SpinnerTextAlignment from '../../../components/spinners/SpinnerTextAlignment'
+import "./index.scss"
+
+const AccountWeek = ({ kFormatter, intl, data }) => {
+  return data !== undefined ? (
+    <StatsWithAreaChart
+      icon={<Box size={21} />}
+      color='warning'
+      stats={formatDisplayNumber(data)}
+      statTitle={intl.formatMessage({id: "account_week"})}
+      className='col-widget'
+    />
+  ) : <Card  className='col-widget d-flex justify-content-center align-items-center'>
+  <SpinnerTextAlignment size={60} />
+</Card>
+}
+
+export default injectIntl(AccountWeek)
