95c4b3c909a7f2c712813017ef3646731265142a
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:32:30 +0700

    Initialize tai/fix_loi_tram_tab_nhan_vien_cot_id

---
+++ src/views/ui-elements/cards/statistics/TotalCompletedSchedule.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Award, Loader } from 'react-feather'
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { injectIntl } from 'react-intl'
+import { Card } from 'reactstrap'
+import { formatDisplayNumber } from '../../../../utility/Utils'
+import SpinnerTextAlignment from '../../../components/spinners/SpinnerTextAlignment'
+
+const TotalCompletedSchedule = ({ kFormatter, intl, data }) => {
+  return data !== null ? (
+    <StatsWithAreaChart
+      icon={<Award size={21} />}
+      color='warning'
+      stats={formatDisplayNumber(data)}
+      statTitle={intl.formatMessage({id: "totalCompletedSchedule"})}
+      className='col-widget'
+    />
+  ) : <Card  className='col-widget d-flex justify-content-center align-items-center'>
+  <SpinnerTextAlignment size={60} />
+</Card>
+}
+
+export default injectIntl(TotalCompletedSchedule)
