e96f7cfa31219eaf1093724ea6c5d39b562cefb0
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:40:50 +0700

    Initialize tai/nhan-tin-cskh

---
+++ src/views/pages/smsCSKH/index.js
@@ -0,0 +1,182 @@
+import React, { useEffect, useState } from 'react'
+import addKeyLocalStorage from '../../../helper/localStorage'
+import TemplateService from '../../../services/template'
+import { toast } from 'react-toastify'
+import { injectIntl } from 'react-intl'
+import { Card } from 'reactstrap'
+import DataTable from 'react-data-table-component'
+import { ChevronDown } from 'react-feather'
+import BasicAutoCompleteDropdown from '../../components/BasicAutoCompleteDropdown/BasicAutoCompleteDropdown'
+import BasicTablePaging from '../../components/BasicTablePaging'
+import MySwitch from '../../components/switch'
+
+const SmsCSKH = ({ intl }) => {
+  const defaultFilter = {
+    filter: {},
+    skip: 0,
+    limit: 20,
+    order: {
+      key: 'createdAt',
+      value: 'desc'
+    }
+  }
+  const [paramsFilter, setParamsFilter] = useState(defaultFilter);
+  const [data, setData] = useState([])
+  const [template, setTemplate] = useState([])
+
+  function getData(params) {
+    const newParams = {
+      ...params
+    }
+    const token = window.localStorage.getItem(addKeyLocalStorage('accessToken'))
+
+    if (token) {
+      const newToken = token.replace(/"/g, '')
+      TemplateService.getCustomerNotifyInfo(newParams, newToken).then((res) => {
+        if (res) {
+          setData(res.data)
+        }
+      })
+    } else {
+      window.localStorage.clear()
+    }
+  }
+
+  function getDataTemplate(params) {
+    const newParams = {
+      ...params
+    }
+    setParamsFilter(newParams)
+    TemplateService.getListTemplate(newParams).then((res) => {
+      if (res) {
+        const { statusCode, data } = res
+        if (statusCode === 200) {
+          const newArr = data.data.map((el) => {
+            return {
+              label: `[${el.messageTemplateType}] ${el.messageTemplateName}`,
+              value: el.messageTemplateId
+            }
+          })
+          setTemplate(newArr)
+        } else {
+          toast.warn(intl.formatMessage({ id: 'failed' }))
+        }
+      } else {
+        setTemplate([])
+      }
+    })
+  }
+
+  useEffect(() => {
+    getData(paramsFilter)
+    getDataTemplate(paramsFilter)
+  }, [])
+
+  const handlePaginations = (page) => {
+    const newParams = {
+      ...paramsFilter,
+      skip: (page - 1) * 20
+    }
+    if(page === 1){
+      getData(paramsFilter)
+      return null
+    }
+    getData(newParams)
+  }
+
+  const CustomPaginations = () => {
+    const lengthItem = data.length
+    return <BasicTablePaging items={lengthItem} handlePaginations={handlePaginations} />
+  }
+
+  const handleInputChange = (value, row, names) => {
+    const newData = {
+      id: row.customerNotifyInfoId,
+      data: {
+        [names] : value
+      }
+    }
+    onUpdateEnableUse(newData)
+  }
+
+  const onUpdateEnableUse = (data) => {
+    TemplateService.updateCustomerNotifyInfo(data).then((res) => {
+      if (res) {
+        const { statusCode } = res
+        if (statusCode === 200) {
+          getData(paramsFilter, true)
+          toast.success(intl.formatMessage({ id: 'actionSuccess' }, { action: intl.formatMessage({ id: 'update' }) }))
+        } else {
+          toast.warn(intl.formatMessage({ id: 'actionFailed' }, { action: intl.formatMessage({ id: 'update' }) }))
+        }
+      }
+    })
+  }
+
+  const columns = [
+    {
+      name: intl.formatMessage({ id: 'purpose' }),
+      minWidth: '500px',
+      maxWidth: '500px',
+      selector: (row) => row?.purpose
+    },
+    {
+      name: intl.formatMessage({ id: 'MessageTemplate' }),
+      minWidth: '300px',
+      maxWidth: '300px',
+      cell: (row) => {
+        const { messageTemplateId, customerNotifyInfoId } = row
+        return (
+          <BasicAutoCompleteDropdown
+            placeholder="Mẫu tin"
+            id={`stationCode_select_${messageTemplateId}`}
+            name="messageTemplateId"
+            options={template}
+            className="w-100"
+            onChange={({ value }) => handleInputChange(value, row, 'messageTemplateId')}
+            value={template.find((el) => el.value === messageTemplateId)}
+            // isDisabled={enableNotifySMS === 1 || enableNotifyZNS === 1 ? false : true}
+          />
+        )
+      }
+    },
+    {
+        name: intl.formatMessage({ id: 'actived' }),
+        minWidth: '150px',
+        maxWidth: '150px',
+        cell: (row) => {
+          const {  activeStatus } = row
+          return (
+            <MySwitch
+              checked={activeStatus === 1 ? true : false}
+              onChange={(e) => {
+                onUpdateEnableUse({
+                  id: row.customerNotifyInfoId,
+                  data: {
+                    activeStatus: e.target.checked ? 1 : 0
+                  }
+                })
+              }}
+            />
+          )
+        }
+      },
+  ]
+
+  return <Card>
+    <div id="users-data-table">
+          <DataTable
+            noHeader
+            // pagination
+            paginationServer
+            className="react-dataTable"
+            columns={columns}
+            sortIcon={<ChevronDown size={10} />}
+            data={data}
+          />
+          {CustomPaginations()}
+        </div>
+  </Card>
+}
+
+export default injectIntl(SmsCSKH)
