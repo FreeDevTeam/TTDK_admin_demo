57f5f4f91fd845eeb88e0f2cc7ccc989dc50281a
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:40:33 +0700

    Initialize nguyen/fix_bug_thiet_lap_doi_tac

---
+++ src/services/reportService.js
@@ -0,0 +1,157 @@
+import Request from "./request";
+
+export default class ReportService {
+  static async getList(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'CustomerStatistical/reportAllStation',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(data)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async DataChart(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'CustomerSchedule/reportTotalByDay',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(data)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async DataScheduleByStation(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'CustomerSchedule/reportTotalScheduleByStation',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(data)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async DataActiveStation(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'Stations/reportAllActiveStation',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(data)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async DataStationArea(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'CustomerSchedule/reportTotalScheduleByStationArea',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(data)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async DatanotActiveStation(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'Stations/reportAllInactiveStation',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(data)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async countScheduleByFilter(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'CustomerSchedule/count',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(result)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async ScheduleChart(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'CustomerSchedule/find',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(data)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async ScheduleReport(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'CustomerSchedule/reportCustomerSchedule',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(result)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+}
