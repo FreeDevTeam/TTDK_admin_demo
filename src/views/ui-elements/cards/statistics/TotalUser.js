2fb4e891c1ed1c6077f773b73a5c95fca2e3372b
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:42:14 +0700

    Initialize tai/fix_bug_wigdet_13.12

---
+++ src/views/ui-elements/cards/statistics/TotalUser.js
@@ -0,0 +1,54 @@
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { useState } from 'react'
+import { Box } from 'react-feather'
+import { injectIntl } from 'react-intl'
+import { Card, Modal, ModalBody, ModalHeader } from 'reactstrap'
+import { STATION_STATUS_FILTER, STATION_STATUS_FILTER_ZERO } from '../../../../constants/dateFormats'
+import { formatDisplayNumber } from '../../../../utility/Utils'
+import SpinnerTextAlignment from '../../../components/spinners/SpinnerTextAlignment'
+import DataTableServerSide from '../../../pages/user/index'
+import "./index.scss"
+
+const TotalUser = ({ kFormatter, intl, data }) => {
+  const DefaultFilter = {
+    filter: {
+      active: STATION_STATUS_FILTER,
+      appUserRoleId: STATION_STATUS_FILTER_ZERO
+    },
+    skip: 0,
+    limit: 10
+  }
+
+  const [open, setOpen] = useState(false)
+
+  return data !== undefined ? (
+    <>
+    <StatsWithAreaChart
+      icon={<Box size={21} />}
+      color='warning'
+      stats={formatDisplayNumber(data)}
+      statTitle={intl.formatMessage({id: "total_user"})}
+      className='col-widget'
+      symboy={true}
+      open={open}
+      setOpen={setOpen}
+    />
+    <Modal
+        isOpen={open}
+        toggle={() => setOpen(false)}
+        className={`modal-dialog-centered `}
+        style={{ maxWidth: window.innerWidth <= 768 ? '100%' : '80%' }}>
+        <ModalHeader toggle={() => setOpen(false)}>{intl.formatMessage({ id: 'total_user' })}</ModalHeader>
+        <ModalBody>
+          <div className="station-active">
+            <DataTableServerSide filterParam={DefaultFilter}/>
+          </div>
+        </ModalBody>
+      </Modal>
+    </>
+  ) : <Card  className='col-widget d-flex justify-content-center align-items-center'>
+  <SpinnerTextAlignment size={60} />
+</Card>
+}
+
+export default injectIntl(TotalUser)
