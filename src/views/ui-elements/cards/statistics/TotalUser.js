8308b30603f6fe9d33a6907773f890ac6cfa57a3
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:32:17 +0700

    Initialize tai/fix_lai_icon_va_test

---
+++ src/views/ui-elements/cards/statistics/TotalUser.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Box, Loader } from 'react-feather'
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { injectIntl } from 'react-intl'
+import "./index.scss";
+import { Card } from 'reactstrap'
+import { formatDisplayNumber } from '../../../../utility/Utils'
+import SpinnerTextAlignment from '../../../components/spinners/SpinnerTextAlignment'
+
+const TotalUser = ({ kFormatter, intl, data }) => {
+  return data !== null ? (
+    <StatsWithAreaChart
+      icon={<Box size={21} />}
+      color='warning'
+      stats={formatDisplayNumber(data)}
+      statTitle={intl.formatMessage({id: "total_user"})}
+      className='col-widget'
+    />
+  ) : <Card  className='col-widget d-flex justify-content-center align-items-center'>
+  <SpinnerTextAlignment size={60} />
+</Card>
+}
+
+export default injectIntl(TotalUser)
