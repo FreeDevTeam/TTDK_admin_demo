d1ce094b111248f566ca6fa8172612a3bc847c36
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:26:54 +0700

    Initialize tai/mau_4_icon_tram

---
+++ src/views/ui-elements/cards/statistics/TotalNotActive.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Home, Loader } from 'react-feather'
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { injectIntl } from 'react-intl'
+import "./index.scss";
+import { Card } from 'reactstrap'
+
+const TotalNotActive = ({ kFormatter, intl, data }) => {
+  const totalStation = data?.totalActiveStation + data?.totalNotActiveStation
+  const notActive = totalStation - data?.totalDeployedStation
+  return data !== null ? (
+    <StatsWithAreaChart
+      icon={<Home size={21} />}
+      color='info'
+      stats={notActive > 0 ? notActive : ''}
+      statTitle={intl.formatMessage({id: "totalNotActive"})}
+      className='col-widget'
+    />
+  ) : <Card  className='col-widget d-flex justify-content-center align-items-center'>
+  <Loader size={60} />
+</Card>
+}
+
+export default injectIntl(TotalNotActive)
