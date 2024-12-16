5ee8060b54ea95a68852d5c088d0006cb5110235
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:25:40 +0700

    Initialize tai/admin_production_tram_so_day_chuyen_nan

---
+++ src/services/appUserConversation.js
@@ -0,0 +1,89 @@
+import request from "./request";
+
+class AppUserConversationService {
+  async createConversation(data = {}) {
+    return request.send({
+      method: "post",
+      path: "/AppUserConversation/insert",
+      data,
+    });
+  }
+
+  async find(data = {}) {
+    return request.send({
+      method: "post",
+      path: "/AppUserConversation/find",
+      data,
+    });
+  }
+
+  async readConversation(data = {}) {
+    return request.send({
+      method: "post",
+      path: "/AppUserConversation/user/readConversation",
+      data,
+    });
+  }
+
+  async getChatLog(data = {}) {
+    return request.send({
+      method: "post",
+      path: "/AppUserChatLog/find",
+      data,
+    });
+  }
+
+  async insertChatLog(data = {}) {
+    return request.send({
+      method: "post",
+      path: "/AppUserChatLog/insert",
+      data,
+    });
+  }
+
+  async deleteChatLogById(id) {
+    return request.send({
+      method: "post",
+      path: "/AppUserChatLog/deleteById",
+      data: {
+        id: id
+      }
+    });
+  }
+
+  async markReadedLog(conversationId) {
+    return request.send({
+      method: "post",
+      path: "/AppUserConversation/updateById",
+      data: {
+        id: conversationId,
+        data: {
+          senderReadMessage: 1,
+        },
+      },
+    });
+  }
+
+  async uploadImage(params) {
+    return new Promise((resolve, reject) => {
+      request.send({
+        method: "POST",
+        path: "/Upload/uploadMediaFile",
+        data: params,
+      })
+        .then((result) => {
+          const { statusCode, data, message } = result;
+          if (statusCode === 200) {
+            resolve(data);
+          } else {
+            throw new Error(message);
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+}
+
+export default new AppUserConversationService();
