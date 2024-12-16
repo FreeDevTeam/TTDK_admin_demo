bdac8facb0c03bd280515767711dff76c3b12aa7
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:36:56 +0700

    Initialize tai/component_paging

---
+++ src/views/components/BasicTablePaging/index.js
@@ -0,0 +1,58 @@
+import { Pagination, PaginationItem, PaginationLink, Input } from 'reactstrap'
+import "./style.scss";
+import { useState } from 'react';
+import { ChevronLeft, ChevronRight } from 'react-feather'
+
+const BasicTablePaging = ({handlePaginations}) => {
+    const Enter = 13
+    const [disabled, setDisabled] = useState(true)
+    const [value, setValue] = useState(1)
+
+    const onKeyDown = (e) =>{
+      if(e.keyCode === Enter){  // click vào phím enter
+        handlePaginations(Number(e.target.value))
+      }
+    }
+
+    const moveToNextPage = () =>{
+      if(value > 9999999){
+        return null
+      }
+      handlePaginations(value + 1)
+      setValue(value + 1)
+    }
+
+    const moveToPreviousPage = () =>{
+      if(value < 2){
+        return null
+      }
+      setValue(value - 1)
+      handlePaginations(value - 1)
+    }
+
+    const handleChange = (value) =>{
+      let newValue = value.replace(/[^0-9]/g, "");
+      setValue(newValue)
+    }
+
+  return (
+    <Pagination className="pagination react-paginate separated-pagination pagination-sm justify-content-end pr-3 mt-1">
+      <div className="prev-item position-relative">
+          <span onClick={() =>moveToPreviousPage()} className='bg_left'>< ChevronLeft /></span>
+      </div>
+      <div>
+        <Input 
+          className='input_paging' 
+          value={value}
+          onKeyDown={(e) => onKeyDown(e)}
+          onChange={(e) => { handleChange(e.target.value) }}
+          />
+      </div>
+      <div className="next-item position-relative">
+          <span onClick={() =>moveToNextPage()} className='bg_right'>< ChevronRight /></span>
+      </div>
+    </Pagination>
+  )
+}
+
+export default BasicTablePaging
