098434d8e5c8e7a2a17de58d691b7de1f633a739
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:27:05 +0700

    Initialize Hung/fix_css_chat

---
+++ src/views/ui-elements/cards/statistics/TotalNotActive.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Home, Loader } from 'react-feather'
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { injectIntl } from 'react-intl'
+import "./index.scss";
+import { Card } from 'reactstrap'
+
+const TotalNotActive = ({ kFormatter, intl, data }) => {
+  let total = 0
+  // const newArray = data.map(value =>{
+  //   const isBool = value.stationBookingConfig.every((item) => item.enableBooking  === 0);
+  //   if(isBool === true){
+  //     total ++
+  //   }
+  // })
+  return data !== null ? (
+    <StatsWithAreaChart
+      icon={<Home size={21} />}
+      stats={total}
+      color='info'
+      statTitle={intl.formatMessage({id: "totalNotActive"})}
+      className='col-widget'
+    />
+  ) : <Card  className='col-widget d-flex justify-content-center align-items-center'>
+  <Loader size={60} />
+</Card>
+}
+
+export default injectIntl(TotalNotActive)
