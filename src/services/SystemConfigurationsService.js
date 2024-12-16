454b280a3ce3119d71314a5aa926c131562a7a99
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:34:53 +0700

    Initialize tai/quanlyTTDK_lich_hen_cot_lien_he

---
+++ src/services/SystemConfigurationsService.js
@@ -0,0 +1,143 @@
+import Request from "./request";
+
+export default class SystemConfigurationsService {
+  static async getPublicSystemConfigurations(data, newToken) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'SystemConfigurations/user/getPublicSystemConfigurations',
+        data: { ...data },
+        query: null,
+        headers: {
+          Authorization: `Bearer ` + newToken,
+        },
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(data)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async updateById(data) {
+    return new Promise((resolve) => {
+      Request.send({
+        method: "POST",
+        path: "SystemConfigurations/updateById",
+        data: data,
+        query: null,
+      }).then((result = {}) => {
+        let { statusCode } = result;
+        if (statusCode === 200) {
+          return resolve({ isSuccess: true })
+        } else {
+          return resolve({ isSuccess: false })
+        }
+      })
+    });
+  }
+  static async handleUpload(data) {
+    return new Promise((resolve) => {
+      Request.send({
+        method: 'POST',
+        path: 'Upload/uploadMediaFile',
+        data: { ...data },
+        query: null
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(result)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async AddAdvertisingBanner(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'SystemPromoBanners/insert',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(result)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async getAdvertisingBannerDetail(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'SystemPromoBanners/findById',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(result)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async getAdvertisingBanner(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'SystemPromoBanners/find',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(result)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async handleUpdateBanner(data) {
+    return new Promise(resolve => {
+      Request.send({
+        method: 'POST',
+        path: 'SystemPromoBanners/updateById',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(result)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+  static async handleDeleteBanner(data) {
+    return new Promise((resolve) => {
+      Request.send({
+        method: 'POST',
+        path: 'SystemPromoBanners/deleteById',
+        data: { ...data },
+        query: null,
+      }).then((result = {}) => {
+        const { statusCode, data } = result
+        if (statusCode === 200) {
+          return resolve(result)
+        } else {
+          return resolve(null)
+        }
+      })
+    })
+  }
+}
