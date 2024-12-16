9a34c9c17b2776e4f31a733ceab794ac432fe56b
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:22:53 +0700

    Initialize tai/chi_tiet_nguoi_dung

---
+++ src/views/pages/user/index.js
@@ -0,0 +1,466 @@
+// @ts-nocheck
+// ** React Imports
+import { Fragment, useState, useEffect, memo } from "react";
+// ** Store & Actions
+import { toast } from "react-toastify";
+import { MoreVertical, Edit, Lock, Shield, RotateCcw } from "react-feather";
+import _ from "lodash";
+import "./index.scss";
+import { useForm } from "react-hook-form";
+import "@styles/react/libs/tables/react-dataTable-component.scss";
+import Service from "../../../services/request";
+import ReactPaginate from "react-paginate";
+import { ChevronDown } from "react-feather";
+import DataTable from "react-data-table-component";
+import { useHistory } from 'react-router-dom'
+import {
+  Card,
+  Input,
+  Label,
+  Row,
+  Col,
+  UncontrolledDropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+  InputGroup,
+  InputGroupButtonDropdown,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Button,
+  FormGroup,
+  Form,
+} from "reactstrap";
+import moment from "moment";
+import { injectIntl } from "react-intl";
+import ResetPasswordUser from "./resetPasswordUser";
+import MySwitch from '../../components/switch';
+
+const statusOptions = [
+  { value: "", label: "all" },
+  { value: 1, label: "ok" },
+  { value: 0, label: "locked" },
+];
+
+const DefaultFilter = {
+  filter: {
+    active: 1,
+    appUserRoleId : 1
+  },
+  skip: 0,
+  limit: 20,
+  order: {
+    key: "createdAt",
+    value: "desc",
+  },
+};
+const DataTableServerSide = ({ intl }) => {
+  // ** Store Vars
+  const serverSideColumns = [
+    {
+      name: "ID",
+      selector: "appUserId",
+      sortable: true,
+      // maxWidth: "80px",
+      minWidth: "80px",
+    },
+    {
+      name: intl.formatMessage({ id: "phoneNumber" }),
+      selector: "phoneNumber",
+      sortable: true,
+      minWidth: "200px",
+    },
+    {
+      name: intl.formatMessage({ id: "fullname" }),
+      sortable: true,
+      minWidth: "200px",
+      cell: (row) => {
+        return (
+          <div>
+            {row.firstName} {row.lastName}
+          </div>
+        );
+      },
+    },
+    {
+      name: intl.formatMessage({ id: "Email" }),
+      selector: "email",
+      sortable: true,
+      minWidth: "200px",
+    },
+    {
+      name: intl.formatMessage({  id: 'lock' }),
+      maxWidth: '230px',
+      minWidth: '230px', 
+      center: true,
+      cell: (row) => {
+        return (
+          <MySwitch
+            checked={row.active === 0 ? true : false}
+            onChange={e => {
+              onUpdateStationEnableUse('AppUsers/updateUserById',{
+                id: row.appUserId,
+                data: {
+                  active: e.target.checked ? 0 : 1
+                }
+              })
+            }}
+          />
+        )
+      }
+    },
+    {
+      name: intl.formatMessage({ id: "createdAt" }),
+      selector: "salary",
+      sortable: true,
+      minWidth: "130px",
+      // maxWidth: "150px",
+      cell: (row) => {
+        const { createdAt } = row;
+
+        return <div>{moment(createdAt).format("hh:mm DD/MM/YYYY")}</div>;
+      },
+    },
+    {
+      name: intl.formatMessage({  id: 'Activation' }),
+      maxWidth: '230px',
+      minWidth: '230px', 
+      center: true,
+      cell: (row) => {
+        return (
+          <MySwitch
+            checked={row.isVerifiedPhoneNumber === 1 ? true : false}
+            onChange={e => {
+              onUpdateStationEnableUse('AppUsers/updateUserById',{
+                id: row.appUserId,
+                data: {
+                  isVerifiedPhoneNumber: e.target.checked ? 1 : 0
+                }
+              })
+            }}
+          />
+        )
+      }
+    },
+    {
+      name: intl.formatMessage({ id: "action" }),
+      selector: "action",
+      cell: (row) => {
+        const { appUserId, lastName, firstName, phoneNumber, active, username } = row;
+        const newPhone = !phoneNumber || phoneNumber === "" ? " " : phoneNumber;
+        return (
+          <div>
+              <span
+                href="/"
+                className="pointer"
+                onClick={(e) => {
+                  e.preventDefault();
+                  history.push("/user/form-user", row)
+                }}
+              >
+                <Edit className="mr-50" size={15} />{" "}
+              </span>
+
+              <span
+              className="pointer ml-1"
+                onClick={(e) => {
+                  e.preventDefault();
+                  let newItem = { ...row };
+                  setIdTrans(newItem);
+                  togglePasswordOpen();
+                }}
+              >
+                <RotateCcw size={15} />
+              </span>
+          </div>
+        );
+      },
+    },
+  ];
+  const [paramsFilter, setParamsFilter] = useState(DefaultFilter);
+  const history = useHistory()
+  // ** States
+  const [modal, setModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(20);
+  const [total, setTotal] = useState(20);
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [isResetPassword, setIsResetPassword] = useState(false);
+  // const [passwordData, setPasswordData] = useState([]);
+  const [sidebarPasswordOpen, setSidebarPasswordOpen] = useState(false);
+  const [idTrans, setIdTrans] = useState(null);
+
+  const togglePasswordOpen = () => {
+    setSidebarPasswordOpen(!sidebarPasswordOpen);
+  };
+  // ** React hook form vars
+  const { register, errors, handleSubmit } = useForm({
+    defaultValues: {},
+  });
+  const [userData, setUserData] = useState({});
+
+  function getData(params, isNoLoading) {
+    const newParams = {
+      ...params,
+    };
+    if (!isNoLoading) {
+      setIsLoading(true);
+    }
+    Object.keys(newParams.filter).forEach((key) => {
+      if (!newParams.filter[key] || newParams.filter[key] === "") {
+        delete newParams.filter[key];
+      }
+    });
+    const token = window.localStorage.getItem("accessToken");
+
+    if (token) {
+      const newToken = token.replace(/"/g, "");
+
+      Service.send({
+        method: "POST",
+        path: "AppUsers/getListlUser",
+        data: newParams,
+        query: null,
+        headers: {
+          Authorization: `Bearer ` + newToken,
+        },
+      }).then((res) => {
+        if (res) {
+          const { statusCode, data, message } = res;
+          setParamsFilter(newParams);
+          if (statusCode === 200) {
+            setTotal(data.total);
+            setItems(data.data);
+          } else {
+            toast.warn(intl.formatMessage({ id: "actionFailed" }));
+          }
+        } else {
+          setTotal(1);
+          setItems([]);
+        }
+        if (!isNoLoading) {
+          setIsLoading(false);
+        }
+      });
+    } else {
+      window.localStorage.clear();
+    }
+  }
+
+  function handleUpdateData(item) {
+    Service.send({
+      method: "POST",
+      path: "AppUsers/updateUserById",
+      data: item,
+      query: null,
+    }).then((res) => {
+      if (res) {
+        const { statusCode, message } = res;
+        if (statusCode === 200) {
+          setUserData({});
+          setModal(false);
+          getData(paramsFilter);
+          toast.success(
+            intl.formatMessage(
+              { id: "actionSuccess" },
+              { action: intl.formatMessage({ id: "update" }) }
+            )
+          );
+        } else {
+          toast.warn(
+            intl.formatMessage(
+              { id: "actionFailed" },
+              { action: intl.formatMessage({ id: "update" }) }
+            )
+          );
+        }
+      }
+    });
+  }
+
+  const getDataSearch = _.debounce((params) => {
+    getData(params, true);
+  }, 1000);
+
+  // ** Get data on mount
+  useEffect(() => {
+    getData(paramsFilter);
+  }, []);
+
+  // ** Function to handle filter
+  const handleFilter = (e) => {
+    const { value } = e.target;
+    setSearchValue();
+    const newParams = {
+      ...paramsFilter,
+      searchText: value,
+        order: {
+          key: "createdAt",
+          value: "desc"
+        }
+    };
+    getDataSearch(newParams);
+  };
+
+  // ** Function to handle Pagination and get data
+  const handlePagination = (page) => {
+    const newParams = {
+      ...paramsFilter,
+      skip: page.selected * paramsFilter.limit,
+    };
+    getData(newParams);
+    setCurrentPage(page.selected + 1);
+  };
+
+  // ** Function to handle per page
+  const handlePerPage = (e) => {
+    const newParams = {
+      ...paramsFilter,
+      limit: parseInt(e.target.value),
+      skip: 0,
+    };
+    getData(newParams);
+    setCurrentPage(1);
+    setRowsPerPage(parseInt(e.target.value));
+  };
+
+  const handleFilterChange = (name, value) => {
+    const newParams = {
+      ...paramsFilter,
+      filter: {
+        ...paramsFilter.filter,
+        [name]: value,
+      },
+      skip: 0,
+    };
+    getData(newParams);
+  };
+
+  // ** Custom Pagination
+  const CustomPagination = () => {
+    const count = 20;
+
+    return (
+      <ReactPaginate
+        previousLabel={""}
+        nextLabel={""}
+        breakLabel="..."
+        pageCount={count || 1}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={2}
+        activeClassName="active"
+        forcePage={currentPage !== 0 ? currentPage - 1 : 0}
+        onPageChange={(page) => handlePagination(page)}
+        pageClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        nextClassName={"page-item next"}
+        previousClassName={"page-item prev"}
+        previousLinkClassName={"page-link"}
+        pageLinkClassName={"page-link"}
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName={
+          "pagination react-paginate separated-pagination pagination-sm justify-content-end pr-1 mt-1"
+        }
+      />
+    );
+  };
+
+  const toggleDropDown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  const handleOnchange = (name, value) => {
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
+  const onUpdateStationEnableUse = (path, data) => {
+    Service.send({
+      method: 'POST', path: path, data: data, query: null
+    }).then(res => {
+      if (res) {
+        const { statusCode } = res
+        if (statusCode === 200) {
+          getData(paramsFilter, true)
+          toast.success(intl.formatMessage({ id: 'actionSuccess' }, { action: intl.formatMessage({ id: "update" }) }))
+        } else {
+          toast.warn(intl.formatMessage({ id: 'actionFailed' }, { action: intl.formatMessage({ id: "update" }) }))
+        }
+      }
+    })
+  }
+
+  return (
+    <Fragment>
+      <Card>
+        <Row className="mx-0 mt-1 mb-50">
+          <Col sm="6"></Col>
+          <Col sm="2">
+            <Input
+              onChange={(e) => {
+                const { name, value } = e.target;
+                handleFilterChange(name, value);
+              }}
+              type="select"
+              value={
+                paramsFilter.filter ? paramsFilter.filter.active || "" : ""
+              }
+              name="active"
+              bsSize="sm"
+            >
+              {statusOptions.map((item) => {
+                return (
+                  <option value={item.value}>
+                    {intl.formatMessage({ id: item.label })}
+                  </option>
+                );
+              })}
+            </Input>
+          </Col>
+          <Col
+            className="d-flex align-items-center justify-content-sm-end mt-sm-0 mt-1"
+            sm="4"
+          >
+            <InputGroup className="input-search-group">
+              <Input
+                placeholder={intl.formatMessage({ id: "Search" })}
+                className="dataTable-filter"
+                type="text"
+                bsSize="sm"
+                id="search-input"
+                value={searchValue}
+                onChange={(e) => {
+                  handleFilter(e);
+                }}
+              />
+            </InputGroup>
+          </Col>
+        </Row>
+        <div id="users-data-table">
+          <DataTable
+            noHeader
+            pagination
+            paginationServer
+            className="react-dataTable"
+            columns={serverSideColumns}
+            sortIcon={<ChevronDown size={10} />}
+            paginationComponent={CustomPagination}
+            data={items}
+            progressPending={isLoading}
+          />
+        </div>
+      </Card>
+
+      <ResetPasswordUser item={idTrans} open={sidebarPasswordOpen} toggleSidebar={togglePasswordOpen}/>
+    </Fragment>
+  );
+};
+
+export default injectIntl(memo(DataTableServerSide));
