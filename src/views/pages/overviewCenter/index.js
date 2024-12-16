7a1ea7609d8ede5df878265fb2800a497207bcaf
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:40:56 +0700

    Initialize tai/trung-tam-lich-hen

---
+++ src/views/pages/overviewCenter/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect, Fragment, useContext } from 'react'
+import { Row, Col, Card, CardBody, CardTitle, CardText } from 'reactstrap'
+import WigdetCenterRevenue from '../../components/wigdetCenterRevenue'
+import { ThemeColors } from '@src/utility/context/ThemeColors'
+import WigdetCenterOrder from '../../components/wigdetCenterOder'
+
+const OverviewCenter = () => {
+  const { colors } = useContext(ThemeColors)
+
+  return (
+    <Fragment>
+      <Row>
+        <Col md="4" xs="12">
+          <WigdetCenterRevenue success={colors.success.main} />
+        </Col>
+        <Col md="2" xs="6">
+          <WigdetCenterOrder warning={colors.warning.main} />
+        </Col>
+      </Row>
+    </Fragment>
+  )
+}
+
+export default OverviewCenter
