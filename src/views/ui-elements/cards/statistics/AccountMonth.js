f1b12661f515f4b603edbdb9ab462b3fda90dca9
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:42:00 +0700

    Initialize tai/wigdet_phuong_tien_da_dang_ki

---
+++ src/views/ui-elements/cards/statistics/AccountMonth.js
@@ -0,0 +1,55 @@
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import moment from 'moment'
+import { useState } from 'react'
+import { Box } from 'react-feather'
+import { injectIntl } from 'react-intl'
+import { Card, Modal, ModalBody, ModalHeader } from 'reactstrap'
+import { formatDisplayNumber } from '../../../../utility/Utils'
+import SpinnerTextAlignment from '../../../components/spinners/SpinnerTextAlignment'
+import DataTableServerSide from '../../../pages/user/index'
+import './index.scss'
+
+const AccountMonth = ({ kFormatter, intl, data }) => {
+  const DefaultFilter = {
+    filter: {},
+    skip: 0,
+    limit: 10,
+    startDate: moment().startOf('month').format('DD/MM/YYYY'),
+    endDate: moment().endOf('month').format('DD/MM/YYYY')
+  }
+
+  const [open, setOpen] = useState(false)
+
+  return data !== undefined ? (
+    <>
+      <StatsWithAreaChart
+        icon={<Box size={21} />}
+        color="warning"
+        stats={formatDisplayNumber(data)}
+        statTitle={intl.formatMessage({ id: 'account_month' })}
+        className="col-widget"
+        symboy={true}
+        open={open}
+        setOpen={setOpen}
+      />
+      <Modal
+        isOpen={open}
+        toggle={() => setOpen(false)}
+        className={`modal-dialog-centered `}
+        style={{ maxWidth: window.innerWidth <= 768 ? '100%' : '80%' }}>
+        <ModalHeader toggle={() => setOpen(false)}>{intl.formatMessage({ id: 'account_month' })}</ModalHeader>
+        <ModalBody>
+          <div className="station-active">
+            <DataTableServerSide filterParam={DefaultFilter} />
+          </div>
+        </ModalBody>
+      </Modal>
+    </>
+  ) : (
+    <Card className="col-widget d-flex justify-content-center align-items-center">
+      <SpinnerTextAlignment size={60} />
+    </Card>
+  )
+}
+
+export default injectIntl(AccountMonth)
