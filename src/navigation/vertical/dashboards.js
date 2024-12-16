cfdba080422296470a94f3275581bc16c7ec9e80
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:35:16 +0700

    Initialize tai/bao_cao_fix_lai_hoat_dong

---
+++ src/navigation/vertical/dashboards.js
@@ -0,0 +1,13 @@
+// @ts-nocheck
+// import { Home, Circle } from 'react-feather'
+
+// export default [
+//   {
+//     id: 'dashboards',
+//     title: 'home',
+//     icon: <Home size={20} />,
+//     badge: 'light-warning',
+//     navLink: '/dashboard/analytics',
+//     permissions: 'VIEW_DASHBOARD'
+//   }
+// ]
