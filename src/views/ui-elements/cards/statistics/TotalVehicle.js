2bf731b28cc778b695cc8f6c719da65b9545a7bb
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:28:49 +0700

    Initialize tai/fix_them_moi_cong_van_thong_bao

---
+++ src/views/ui-elements/cards/statistics/TotalVehicle.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Command, Loader} from 'react-feather'
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { injectIntl } from 'react-intl'
+import { Card } from 'reactstrap'
+
+const TotalVehicle = ({ kFormatter, intl, data }) => {
+  return data !== null ? (
+    <StatsWithAreaChart
+      icon={<Command size={21} />}
+      color='warning'
+      stats={data}
+      statTitle={intl.formatMessage({id: "totalVehicle"})}
+      className='col-widget'
+    />
+  ) : <Card  className='col-widget d-flex justify-content-center align-items-center'>
+  <Loader size={60} />
+</Card>
+}
+
+export default injectIntl(TotalVehicle)
