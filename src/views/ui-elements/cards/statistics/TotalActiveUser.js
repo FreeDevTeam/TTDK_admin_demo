99d3a0d85a2506ec83362cf85ccee780218d3f3e
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:27:31 +0700

    Initialize tai/chi_tiet_user_bo_validate

---
+++ src/views/ui-elements/cards/statistics/TotalActiveUser.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Aperture, Loader } from 'react-feather'
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { injectIntl } from 'react-intl'
+import { Card } from 'reactstrap'
+
+const TotalActiveUser = ({ kFormatter, intl, data }) => {
+
+  return data !== null ? (
+    <StatsWithAreaChart
+      icon={<Aperture size={21} />}
+      color='warning'
+      stats={data}
+      statTitle={intl.formatMessage({id: "user_active"})}
+      className='col-widget'
+    />
+  ) : <Card  className='col-widget d-flex justify-content-center align-items-center'>
+  <Loader size={60} />
+</Card>
+}
+
+export default injectIntl(TotalActiveUser)
