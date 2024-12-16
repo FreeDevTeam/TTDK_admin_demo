c9d27717dce8300111bece9db3214d861207bdbc
caonguyencmg <85693671+caonguyencmg@users.noreply.github.com>
2024-12-16 09:28:51 +0700

    Initialize tai/bao_cao_loai_lich_hen

---
+++ src/services/notificationService.js
@@ -0,0 +1,133 @@
+import Request from "./request";
+
+export default class NotificationService {
+    static async getList(data, newToken) {
+        return new Promise(resolve => {
+            Request.send({
+                method: 'POST',
+                path: 'StaffNotification/find',
+                data: { ...data },
+                query: null,
+                headers: {
+                    Authorization: `Bearer ` + newToken,
+                },
+            }).then((result = {}) => {
+                const { statusCode, data } = result
+                if (statusCode === 200) {
+                    return resolve(result)
+                } else {
+                    return resolve(null)
+                }
+            })
+        })
+    }
+    static async deleteNotificationById(id) {
+        return new Promise(resolve=>{
+          Request.send({
+            method: 'POST',
+            path: 'StaffNotification/deleteById',
+            data: {
+                id: id
+            },
+            query: null,
+          }).then((result = {})=>{
+            const { statusCode } = result
+            if(statusCode === 200) {
+                return resolve(result)
+            }else{
+                return resolve(null)
+            }
+          })
+        })
+      }
+      static async getDetailById(id) {
+        return new Promise(resolve=>{
+          Request.send({
+            method: 'POST',
+            path: 'StaffNotification/findById',
+            data: {
+                id: id
+            },
+            query: null,
+          }).then((result = {})=>{
+            const { statusCode } = result
+            if(statusCode === 200) {
+                return resolve(result)
+            }else{
+                return resolve(null)
+            }
+          })
+        })
+      }
+    static async update(data, newToken) {
+        return new Promise(resolve => {
+            Request.send({
+                method: 'POST',
+                path: 'StaffNotification/insert',
+                data: { ...data },
+                query: null,
+                headers: {
+                    Authorization: `Bearer ` + newToken,
+                },
+            }).then((result = {}) => {
+                const { statusCode, data } = result
+                if (statusCode === 200) {
+                    return resolve(result)
+                } else {
+                    return resolve(null)
+                }
+            })
+        })
+    }
+    static async uploadImage(data) {
+        return new Promise(resolve => {
+            Request.send({
+                method: 'POST',
+                path: 'Upload/uploadMediaFile',
+                data: { ...data },
+                query: null,
+            }).then((result = {}) => {
+                const { statusCode, data } = result
+                if (statusCode === 200) {
+                    return resolve(result)
+                } else {
+                    return resolve(null)
+                }
+            })
+        })
+    }
+    static async handleUpdate(data) {
+        return new Promise(resolve => {
+            Request.send({
+                method: 'POST',
+                path: 'StaffNotification/updateById',
+                data: { ...data },
+                query: null,
+            }).then((result = {}) => {
+                const { statusCode, data } = result
+                if (statusCode === 200) {
+                    return resolve(result)
+                } else {
+                    return resolve(null)
+                }
+            })
+        })
+    }
+    static async insertNotification(data) {
+        return new Promise(resolve => {
+            Request.send({
+                method: 'POST',
+                path: 'StaffNotification/insert',
+                data: { ...data },
+                query: null,
+            }).then((result = {}) => {
+                const { statusCode, data } = result
+                if (statusCode === 200) {
+                    return resolve(result)
+                } else {
+                    return resolve(null)
+                }
+            })
+        })
+    }
+}
